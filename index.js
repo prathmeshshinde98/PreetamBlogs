c0c1dfdd3941314a619052083c0d79fc3700e5bc
Prathmesh Shinde <random-coder51@users.noreply.github.com>
2021-04-14 14:38:56 +0530

    formatting

--- index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("scroll",handleScroll);
-function handleScroll(){
-    // document.body.style.marginTop =  `${pageYOffset}px`;
-    // console.log(document.body.style.marginTop)
-    // console.log(pageYOffset)
-}
